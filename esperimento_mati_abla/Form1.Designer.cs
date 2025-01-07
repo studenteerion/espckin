a4cbc451d9337e5630dcb63a27f27d772ea01454
Deyolf <colombofabio06@gmail.com>
2025-01-07 14:18:04 +0100

    upload

---
+++ esperimento_mati_abla/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace esperimento_mati_abla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(437, 119);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ esperimento_mati_abla/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Newtonsoft.Json;
+
+
+namespace esperimento_mati_abla
+{
+
+    public partial class Form1 : Form
+
+    {
+        
+
+        public class Info
+        {
+            public string targa { get; set; }
+            public string mail { get; set; }
+            public string zona_accesso { get; set; }
+        }
+
+        public List<Info> info=new List<Info>();
+
+
+        public string targa = "aa11aa";
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // Esegui una richiesta GET o POST
+            string url = "http://127.0.0.1:9000/" + targa;  // Modifica con l'URL desiderato
+            string result = await MakeHttpRequest(url);
+            label1.Text = result;
+        }
+
+        
+        private async Task<string> MakeHttpRequest(string url)
+        {
+            // Crea un'istanza di HttpClient
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    
+                    // Inviamo una richiesta GET (modifica a POST se necessario)
+                    HttpResponseMessage response = await client.GetAsync(url);  // Usa PostAsync per le richieste POST
+                    
+                    var resultString= await response.Content.ReadAsStringAsync();
+                    
+                    response.EnsureSuccessStatusCode();  // Lancia un'eccezione se il codice di stato non è OK
+                    var professore = JsonConvert.DeserializeObject<Info>(resultString);
+                    // Ottieni la risposta come stringa
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    return responseBody;
+                }
+                catch (Exception ex)
+                {
+                    // Gestione degli errori, ad esempio se il server non è raggiungibile
+                    return $"Errore: {ex.Message}";
+                }
+            }
+        }
+
+        
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
