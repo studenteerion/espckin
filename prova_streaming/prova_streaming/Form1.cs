cc1acf9724f832b5fe61f2738d688fd7604c934a
Matilde8889 <ravasio.matilde.studente@itispaleocapa.it>
2025-01-27 15:26:30 +0100

    frame capturing

---
+++ prova_streaming/prova_streaming/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MjpegProcessor;
+
+namespace prova_streaming
+{
+    public partial class Form1 : Form
+    {
+        private MjpegDecoder mjpeg;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getframe();
+        }
+
+        private void getframe()
+        {
+            mjpeg = new MjpegDecoder();
+            mjpeg.FrameReady += mjpeg_FrameReady;
+            mjpeg.Error += mjpeg_Error;
+
+            mjpeg.ParseStream(new Uri("http://192.168.103.50:81/stream"));
+
+            MessageBox.Show("Frame Catturato");
+        }
+
+
+        private void mjpeg_FrameReady(object sender, FrameReadyEventArgs e)
+        {
+            Bitmap bmp;
+            using (var ms = new MemoryStream(e.FrameBuffer))
+            {
+                bmp = new Bitmap(ms);
+            }
+
+            System.Drawing.Image newImg = (System.Drawing.Image)bmp.Clone();
+            bmp.Dispose();
+
+            newImg.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+            System.Drawing.Graphics gr = System.Drawing.Graphics.FromImage(newImg);
+            string drawString = "camera!";
+            System.Drawing.Font drawFont = new System.Drawing.Font("Arial", 12);
+            System.Drawing.SolidBrush drawBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+
+            float x = 560.0f;
+            float y = 460.0f;
+            gr.FillRectangle(new System.Drawing.SolidBrush(System.Drawing.Color.Green), new System.Drawing.Rectangle(545, 465, 10, 10));
+
+            gr.DrawString(drawString, drawFont, drawBrush, x, y);
+
+            gr.DrawString(DateTime.Now.ToLocalTime().ToString(), drawFont, drawBrush, 12, y);
+
+            pictureBox1.Image = newImg; //disegna nella box
+
+            drawFont.Dispose();
+            drawBrush.Dispose();
+            gr.Dispose();
+        }
+
+        private void mjpeg_Error(object sender, MjpegProcessor.ErrorEventArgs e)
+        {
+            MessageBox.Show(e.Message);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
