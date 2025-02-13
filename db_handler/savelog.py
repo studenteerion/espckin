4eaeff879c197758ace024ec52bd20217797851e
Deyolf <colombofabio06@gmail.com>
2025-02-13 18:46:20 +0100

    test
    10 min Fabio
    test

--- db_handler/savelog.py
+++ db_handler/savelog.py
@@ -20,14 +20,14 @@ def syslog():
 def savelog(user,route):
     timestamp = datetime.now().strftime('%Y/%m/%d - %H:%M:%S')
     user_str = "{"+f' "user": "{json.loads(user).get("user")}", "key": "{json.loads(user).get("key")}" '+"}"
-    new_data = f'\n    "timestamp": "{timestamp}",\n    "client_auth": {user_str},\n    "request_route": "{route}"'
+    new_data = json.loads("{"+f'"timestamp": "{timestamp}", "client_auth": {user}, "request_route": "{route}"'+"}")
 
     if os.path.exists(FILE_PATH):
         with open(FILE_PATH, "r+") as file:
             file.seek(0, 2)
             pos = file.tell()
             file.seek(pos-5)
-            file.write(",\n  {"+new_data + "\n  }\n]\n")
+            file.write(json.dumps(new_data, indent=2, ensure_ascii=False))
             user = json.loads(user)
             print (f"{syslog()}{Fore.LIGHTGREEN_EX}client_auth{Fore.RESET} {user.get("user")}-{user.get("key")} - - {Fore.LIGHTGREEN_EX}route{Fore.RESET} {route}")
 
