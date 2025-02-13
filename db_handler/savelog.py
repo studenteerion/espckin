0c265f3f31754b3674f48317cba56ea69e228bed
Deyolf <colombofabio06@gmail.com>
2025-02-13 11:16:24 +0100

    log update
    40 min Fabio
    -Miglioramento file output con indentazione fatta manualmente

--- db_handler/savelog.py
+++ db_handler/savelog.py
@@ -6,28 +6,28 @@ from colorama import init, Fore
 init()
 
 FILE_PATH = "./log.json"
+day = datetime.now().strftime('%d')
+month = datetime.now().strftime('%m')
+year = datetime.now().strftime('%Y')
+hour = datetime.now().strftime('%H')
+min = datetime.now().strftime('%M')
+sec = datetime.now().strftime('%S')
 
 def syslog():
-    day = datetime.now().strftime('%d')
-    month = datetime.now().strftime('%m')
-    year = datetime.now().strftime('%Y')
-    hour = datetime.now().strftime('%H')
-    min = datetime.now().strftime('%M')
-    sec = datetime.now().strftime('%S')
     composed = f"[{year}/{month}/{day} - {hour}:{min}:{sec}] - {Fore.LIGHTBLUE_EX}SYS{Fore.RESET} - "
     return composed
 
 def savelog(user,route):
     timestamp = datetime.now().strftime('%Y/%m/%d - %H:%M:%S')
-
-    new_data = {"timestamp": timestamp, "client_auth": user,"request_route": route}
+    user_str = "{"+f' "user": "{json.loads(user).get("user")}", "key": "{json.loads(user).get("key")}" '+"}"
+    new_data = f'\n    "timestamp": "{timestamp}",\n    "client_auth": {user_str},\n    "request_route": "{route}"'
 
     if os.path.exists(FILE_PATH):
         with open(FILE_PATH, "r+") as file:
             file.seek(0, 2)
             pos = file.tell()
-            file.seek(pos-1)
-            file.write(","+json.dumps(new_data) + "\n")
+            file.seek(pos-5)
+            file.write(",\n  {"+new_data + "\n  }\n]\n")
             user = json.loads(user)
             print (f"{syslog()}{Fore.LIGHTGREEN_EX}client_auth{Fore.RESET} {user.get("user")}-{user.get("key")} - - {Fore.LIGHTGREEN_EX}route{Fore.RESET} {route}")
 
