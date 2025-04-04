4326138a633c1a4df7da6523989691ab6d531317
omaraabla <omara.abla.studente@itispaleocapa.it>
2025-04-04 16:09:11 +0200

    camera db c#

---
+++ Cameradb/Cameradb/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Cameradb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ip_txt = new System.Windows.Forms.TextBox();
+            this.nome_txt = new System.Windows.Forms.TextBox();
+            this.descrizione_txt = new System.Windows.Forms.TextBox();
+            this.coordinate_txt = new System.Windows.Forms.TextBox();
+            this.aggiungi_btn = new System.Windows.Forms.Button();
+            this.cameraListBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ip_txt
+            // 
+            this.ip_txt.Location = new System.Drawing.Point(77, 116);
+            this.ip_txt.Name = "ip_txt";
+            this.ip_txt.Size = new System.Drawing.Size(100, 20);
+            this.ip_txt.TabIndex = 0;
+            // 
+            // nome_txt
+            // 
+            this.nome_txt.Location = new System.Drawing.Point(198, 116);
+            this.nome_txt.Name = "nome_txt";
+            this.nome_txt.Size = new System.Drawing.Size(100, 20);
+            this.nome_txt.TabIndex = 1;
+            // 
+            // descrizione_txt
+            // 
+            this.descrizione_txt.Location = new System.Drawing.Point(319, 116);
+            this.descrizione_txt.Name = "descrizione_txt";
+            this.descrizione_txt.Size = new System.Drawing.Size(100, 20);
+            this.descrizione_txt.TabIndex = 2;
+            // 
+            // coordinate_txt
+            // 
+            this.coordinate_txt.Location = new System.Drawing.Point(449, 116);
+            this.coordinate_txt.Name = "coordinate_txt";
+            this.coordinate_txt.Size = new System.Drawing.Size(100, 20);
+            this.coordinate_txt.TabIndex = 3;
+            // 
+            // aggiungi_btn
+            // 
+            this.aggiungi_btn.Location = new System.Drawing.Point(622, 116);
+            this.aggiungi_btn.Name = "aggiungi_btn";
+            this.aggiungi_btn.Size = new System.Drawing.Size(100, 20);
+            this.aggiungi_btn.TabIndex = 4;
+            this.aggiungi_btn.Text = "AGGIUNGI";
+            this.aggiungi_btn.UseVisualStyleBackColor = true;
+            this.aggiungi_btn.Click += new System.EventHandler(this.aggiungi_btn_Click);
+            // 
+            // cameraListBox
+            // 
+            this.cameraListBox.FormattingEnabled = true;
+            this.cameraListBox.Location = new System.Drawing.Point(533, 187);
+            this.cameraListBox.Name = "cameraListBox";
+            this.cameraListBox.Size = new System.Drawing.Size(189, 173);
+            this.cameraListBox.TabIndex = 5;
+            this.cameraListBox.SelectedIndexChanged += new System.EventHandler(this.cameraListBox_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cameraListBox);
+            this.Controls.Add(this.aggiungi_btn);
+            this.Controls.Add(this.coordinate_txt);
+            this.Controls.Add(this.descrizione_txt);
+            this.Controls.Add(this.nome_txt);
+            this.Controls.Add(this.ip_txt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ip_txt;
+        private System.Windows.Forms.TextBox nome_txt;
+        private System.Windows.Forms.TextBox descrizione_txt;
+        private System.Windows.Forms.TextBox coordinate_txt;
+        private System.Windows.Forms.Button aggiungi_btn;
+        private System.Windows.Forms.ListBox cameraListBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Cameradb/Cameradb/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Cameradb
+{
+    public partial class Form1 : Form
+    {
+        // URL di base dell'API
+        private const string BASE_URL = "http://192.168.103.11:5000/api";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Funzione per aggiungere una fotocamera (logica separata in un metodo asincrono)
+        private async Task AggiungiCameraAsync()
+        {
+            // Controlla se tutti i campi sono riempiti
+            if (string.IsNullOrEmpty(ip_txt.Text) || string.IsNullOrEmpty(nome_txt.Text) || string.IsNullOrEmpty(descrizione_txt.Text) || string.IsNullOrEmpty(coordinate_txt.Text))
+            {
+                MessageBox.Show("Tutti i campi devono essere compilati.");
+                return;
+            }
+
+            // Creiamo un oggetto con i dati della fotocamera
+            var cameraData = new
+            {
+                ip = ip_txt.Text,
+                name = nome_txt.Text,
+                description = descrizione_txt.Text,
+                coordinates = coordinate_txt.Text
+            };
+
+
+            // Serializza l'oggetto in formato JSON
+            string jsonString = JsonConvert.SerializeObject(cameraData);
+
+            // Mostra il JSON in una MessageBox
+            MessageBox.Show("JSON da inviare: " + jsonString, "Contenuto JSON", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            var jsonContent = new StringContent(
+                JsonConvert.SerializeObject(cameraData),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            // Inizializza HttpClient per inviare la richiesta POST
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.PostAsync($"{BASE_URL}/add_camera", jsonContent);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show("Fotocamera aggiunta con successo.", "Successo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        // Leggi il corpo della risposta in caso di errore per ottenere più dettagli
+                        string errorResponse = await response.Content.ReadAsStringAsync();
+                        MessageBox.Show("Errore nell'aggiungere la fotocamera: " + response.StatusCode + "\n" + errorResponse, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errore nella richiesta: " + ex.ToString(), "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+
+            }
+        }
+
+        // Funzione per ottenere tutte le fotocamere
+        public async Task GetAllCameras()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Esegui la richiesta GET per ottenere tutte le fotocamere
+                    HttpResponseMessage response = await client.GetAsync($"{BASE_URL}/get_all_cameras");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Leggi la risposta JSON
+                        string responseData = await response.Content.ReadAsStringAsync();
+
+                        // Deserializza la risposta JSON in una lista di fotocamere
+                        var cameras = JsonConvert.DeserializeObject<List<Camera>>(responseData);
+
+                        // Pulisce la ListBox prima di aggiungere nuovi elementi
+                        cameraListBox.Items.Clear();
+
+                        // Aggiungi ogni fotocamera alla ListBox
+                        foreach (var camera in cameras)
+                        {
+                            // Puoi decidere di visualizzare solo alcuni campi, come il nome o l'IP
+                            cameraListBox.Items.Add($"Name: {camera.Name}, IP: {camera.Ip}, Coordinates: {camera.Coordinates}");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Errore nel recupero delle fotocamere: " + response.StatusCode, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errore nella richiesta: " + ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Funzione per aggiornare una fotocamera
+        public async Task UpdateCamera(int cameraId)
+        {
+            var cameraData = new
+            {
+                id = cameraId,
+                ip = "192.168.1.2",
+                name = "Updated Camera",
+                description = "Updated description for the camera",
+                coordinates = "40.7306,-73.9352"
+            };
+
+            var jsonContent = new StringContent(
+                JsonConvert.SerializeObject(cameraData),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.PutAsync($"{BASE_URL}/update_camera", jsonContent);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show("Fotocamera aggiornata con successo.", "Successo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Errore nell'aggiornare la fotocamera: " + response.StatusCode, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errore nella richiesta: " + ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Funzione per eliminare una fotocamera
+        public async Task DeleteCamera(int cameraId)
+        {
+            var cameraData = new
+            {
+                id = cameraId
+            };
+
+            var jsonContent = new StringContent(
+                JsonConvert.SerializeObject(cameraData),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Usa HttpRequestMessage per DELETE con body
+                    var request = new HttpRequestMessage(HttpMethod.Delete, $"{BASE_URL}/delete_camera")
+                    {
+                        Content = jsonContent
+                    };
+
+                    HttpResponseMessage response = await client.SendAsync(request);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show("Fotocamera eliminata con successo.", "Successo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Errore nell'eliminare la fotocamera: " + response.StatusCode, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errore nella richiesta: " + ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Funzione per avviare il test
+        public async Task TestRoutes()
+        {
+            // Aggiungi una fotocamera
+            await AggiungiCameraAsync();  // Chiamato il metodo asincrono
+
+            // Ottieni tutte le fotocamere
+            await GetAllCameras();
+
+            // Aggiorna una fotocamera
+            int cameraId = 1; // Assicurati di sostituirlo con un ID valido
+            await UpdateCamera(cameraId);
+
+            // Ottieni tutte le fotocamere dopo l'aggiornamento
+            await GetAllCameras();
+
+            // Elimina una fotocamera
+            await DeleteCamera(cameraId);
+
+            // Ottieni tutte le fotocamere dopo l'eliminazione
+            await GetAllCameras();
+        }
+
+        // Funzione per invocare il test
+        private async void test_btn_Click(object sender, EventArgs e)
+        {
+            await TestRoutes(); // Avvia il test dei metodi asincroni
+        }
+
+        // Funzione per il pulsante "Aggiungi Fotocamera"
+        private async void aggiungi_btn_Click(object sender, EventArgs e)
+        {
+            await AggiungiCameraAsync();
+        }
+
+        // Funzione per il pulsante "Mostra tutte le fotocamere"
+        private async void showAll_btn_Click(object sender, EventArgs e)
+        {
+            await GetAllCameras();
+        }
+
+        private void cameraListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await GetAllCameras();
+        }
+    }
+
+    public class Camera
+    {
+        public string Ip { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Coordinates { get; set; }
+    }
+}
