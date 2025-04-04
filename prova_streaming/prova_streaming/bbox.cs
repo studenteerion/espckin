601e00ac3f8d31b1b42fc453d783d4bead806f99
Matilde8889 <ravasio.matilde.studente@itispaleocapa.it>
2025-04-04 16:06:48 +0200

    websocket

---
+++ prova_streaming/prova_streaming/bbox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prova_streaming
+{
+    public class bbox
+    {
+
+        public int x_min;
+        public int y_min;
+        public int x_max;
+        public int y_max;
+
+    }
+}
+
+
+/*'bounding_box': {
+    'x_min': 100,
+                'y_min': 50,
+                'x_max': 200,
+                'y_max': 150
+            }*/
