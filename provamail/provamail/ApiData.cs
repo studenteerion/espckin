8a65239e67863967021adee15c1b3c1be535f9cd
Matilde8889 <ravasio.matilde.studente@itispaleocapa.it>
2025-05-19 16:12:35 +0200

    Invio richieste al server e modifica della mail

---
+++ provamail/provamail/ApiData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provamail
+{
+    internal class ApiData
+    {
+        public string Cognome { get; set; }
+        public string id_professore { get; set; }
+        public string Mail { get; set; }
+        public string Nome { get; set; }
+        public string Targa { get; set; }
+        public string zona_accesso { get; set; }
+    }
+}
---
+++ provamail/provamail/Targa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provamail
+{
+    internal class Targa
+    {
+        // nome della camera
+        public string camera_name { get; set; }
+
+        public string camera_location { get; set; } 
+        
+        public string camera_description { get; set; }
+
+        //targa
+        public string plate_number { get; set; }
+
+        //data
+        public string timestamp { get; set; }
+
+        public string image { get; set; }
+
+        //bbox 
+        public bbox bounding_box { get; set; }
+
+        public class bbox
+        {
+            public int x_min { get; set; }
+
+            public int y_min { get; set; }
+
+            public int x_max { get; set; }
+
+            public int y_max { get; set; }
+        }
+    }
+}
